b6d95f299562d4aa46c193b6fdb44a9a7b382633
PratikSavla <pratiksavla07@gmail.com>
2022-06-16 18:20:51 +0530

    remove nlp dependency

--- src/main/java/com/freudas/dev/config/NLPPipeline.java
+++ src/main/java/com/freudas/dev/config/NLPPipeline.java
@@ -1,39 +1,39 @@
 package com.freudas.dev.config;
-//nlpPipeline.java
-import java.util.Properties;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;
-import org.springframework.context.annotation.Configuration;
+////nlpPipeline.java
+//import java.util.Properties;
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//import edu.stanford.nlp.ling.CoreAnnotations;
+//import edu.stanford.nlp.pipeline.Annotation;
+//import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+//import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+//import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
+//import edu.stanford.nlp.trees.Tree;
+//import edu.stanford.nlp.util.CoreMap;
+//import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class NLPPipeline {
-    private final StanfordCoreNLP pipeline;
-    public NLPPipeline()
-    {
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-        pipeline = new StanfordCoreNLP(props);
-    }
-    public double estimatingSentiment(String text)
-    {
-        Double sentimentInt = 0.0;
-        String sentimentName;
-        int sentenceCount = 0;
-        Annotation annotation = pipeline.process(text);
-        for(CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class))
-        {
-            Tree tree = sentence.get(SentimentAnnotatedTree.class);
-            sentimentInt += RNNCoreAnnotations.getPredictedClass(tree);
-            sentimentName = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
-            sentenceCount++;
-            System.out.println(sentimentName + "\t" + sentimentInt + "\t" + sentence);
-        }
-        return sentimentInt/sentenceCount;
-    }
+//    private final StanfordCoreNLP pipeline;
+//    public NLPPipeline()
+//    {
+//        Properties props = new Properties();
+//        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+//        pipeline = new StanfordCoreNLP(props);
+//    }
+//    public double estimatingSentiment(String text)
+//    {
+//        Double sentimentInt = 0.0;
+//        String sentimentName;
+//        int sentenceCount = 0;
+//        Annotation annotation = pipeline.process(text);
+//        for(CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class))
+//        {
+//            Tree tree = sentence.get(SentimentAnnotatedTree.class);
+//            sentimentInt += RNNCoreAnnotations.getPredictedClass(tree);
+//            sentimentName = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+//            sentenceCount++;
+//            System.out.println(sentimentName + "\t" + sentimentInt + "\t" + sentence);
+//        }
+//        return sentimentInt/sentenceCount;
+//    }
 }
